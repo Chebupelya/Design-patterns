669fa48b0fcd35a4c05284775781f073dc085964
Nikita <nikitailyin186@gmail.com>
2023-03-12 19:59:34 +0300

    commit

---
+++ Lab_03/Lab_03_Patterns/Lab_03_Patterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_03_Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
